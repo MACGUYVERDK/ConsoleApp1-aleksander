7b63327fe9ed78dae46cf10888475d7dc1187e41
MACGUYVERDK <macguyverdk@hotmail.com>
2024-02-24 09:56:29 +0100

    Add project files.

---
+++ ConsoleApp1 aleksander/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string name = "Ewa";
+var sex = "woman";
+var age = 25;
+
+if (sex == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (age < 18 && sex == "man")
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
